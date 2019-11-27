11475bc1b05a59fd86156860cca84ede2fbcd3b0
Ismael AlShiab <ismaelalshiab@cmail.carleton.ca>
2019-11-26 23:30:30 -0500

    update routes

--- CA/ComScripts/routes.sh
+++ CA/ComScripts/routes.sh
@@ -2,6 +2,6 @@
 # WIFI_INTERFACE name is wlan0 if PREDICTABLE NAMES are not enabled.
 # WIFI_INTERFACE name is wlxMAC if PREDICTABLE NAMES are enabled.
 # Creating static routes should be as needed, if noticed inconsistent routing with some parts.
-sudo ip route add IP-TA/32 via IP_NEXT_HOP_TOWARD_TA dev WIFI_INTERFACE_NAME
+sudo ip route add IP-TA/32 via IP_CA_NEXT_HOP_TOWARD_TA dev WIFI_INTERFACE_NAME
 
 
--- TA/ComScripts/routes.sh
+++ TA/ComScripts/routes.sh
@@ -2,6 +2,6 @@
 # WIFI_INTERFACE name is wlan0 if PREDICTABLE NAMES are not enabled.
 # WIFI_INTERFACE name is wlxMAC if PREDICTABLE NAMES are enabled.
 # Creating static routes should be as needed, if noticed inconsistent routing with some parts.
-sudo ip route add IP/NETMASK via IP_NEXT_HOP_TOWARD_DESTINATION dev WIFI_INTERFACE_NAME
+sudo ip route add IP-CA/32 via IP_CA_NEXT_HOP_TOWARD_DESTINATION_CA dev WIFI_INTERFACE_NAME
 
 
